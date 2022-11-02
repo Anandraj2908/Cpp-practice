e9f36df8c7f75a3d74749d80e7b01a7835b79d75
Anand Raj <anandraj.solutions@gmail.com>
2022-11-02 22:02:48 +0530

    initial commit

---
+++ Recursion/checkNumber.cpp
@@ -0,0 +1,43 @@
+#include<iostream>
+
+using namespace std;
+
+bool checkNumber(int input[], int size, int x) {
+  /* Don't write main().
+     Don't read input, it is passed as function argument.
+     Return output and don't print it.
+     Taking input and printing output is handled automatically.
+  */
+  if(size<=0){
+      return false;
+  }
+  if(input[size-1]==x){
+      return true;
+  }
+  return checkNumber(input,size-1,x);
+}
+
+
+int main(){
+    int n;
+    cin >> n;
+  
+    int *input = new int[n];
+    
+    for(int i = 0; i < n; i++) {
+        cin >> input[i];
+    }
+    
+    int x;
+    
+    cin >> x;
+    
+    if(checkNumber(input, n, x)) {
+        cout << "true" << endl;
+    }
+    else {
+        cout << "false" << endl;
+    }
+}
+
+
