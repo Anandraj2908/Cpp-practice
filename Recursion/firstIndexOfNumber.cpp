2566875c98e79a98e25a2480f0d721b6feae30d4
Anand Raj <anandraj.solutions@gmail.com>
2022-11-03 22:58:08 +0530

    Initial commit

---
+++ Recursion/firstIndexOfNumber.cpp
@@ -0,0 +1,50 @@
+#include<iostream>
+
+using namespace std;
+
+int firstIndex(int input[], int size, int x) {
+  /* Don't write main().
+     Don't read input, it is passed as function argument.
+     Return output and don't print it.
+     Taking input and printing output is handled automatically.
+  */
+   
+    
+    //Base case 
+    if(size<0){
+        return -1;
+    }
+    
+    //small Calculation 
+    if(input[0]==x){
+        return 0;
+    }
+    int ans=firstIndex(input+1,size-1,x);
+    if(ans==-1){
+        return -1;
+    }
+    else{
+        return ans+1;
+    }
+}
+
+
+int main(){
+    int n;
+    cin >> n;
+  
+    int *input = new int[n];
+    
+    for(int i = 0; i < n; i++) {
+        cin >> input[i];
+    }
+    
+    int x;
+    
+    cin >> x;
+    
+    cout << firstIndex(input, n, x) << endl;
+
+}
+
+
