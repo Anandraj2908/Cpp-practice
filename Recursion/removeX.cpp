f3dc5e655f303c24fb31bae2d6420af2794580b1
Anand Raj <anandraj.solutions@gmail.com>
2022-11-17 22:46:20 +0530

    Initial commit

---
+++ Recursion/removeX.cpp
@@ -0,0 +1,30 @@
+// Change in the given string itself. So no need to return or print anything
+#include <iostream>
+
+// Change in the given string itself. So no need to return or print anything
+#include <bits/stdc++.h>
+void removeX(char input[]) {
+    // Write your code here
+    if(input[0]=='\0'){
+        return;
+    }
+    
+    removeX(input+1);
+    
+    //small calculation
+    if(input[0]=='x'){
+        for(int i=0;i<=strlen(input);i++){
+            input[i]=input[i+1];
+        }
+    }
+}
+
+
+using namespace std;
+
+int main() {
+    char input[100];
+    cin.getline(input, 100);
+    removeX(input);
+    cout << input << endl;
+}
